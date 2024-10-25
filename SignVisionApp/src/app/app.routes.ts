f16393561e0b0691cc2940c0f459ff45e25ee618
JESS-L-ROBOT <zickgrafj@gmail.com>
2024-10-25 11:16:09 +0200

    Konfigurationsstuff

--- SignVisionApp/src/app/app.routes.ts
+++ SignVisionApp/src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ComponentImpressumComponent } from './component-impressum/component-impressum.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: AppComponent},
+    {path:'impressum', component:ComponentImpressumComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
