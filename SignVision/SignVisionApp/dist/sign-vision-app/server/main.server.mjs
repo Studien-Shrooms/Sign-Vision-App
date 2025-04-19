9063f5a894db09b8fce60f6f24b8500c611927c6
Charl0tte123 <charlotteobauer@gmail.com>
2025-04-19 19:26:25 +0200

    Update module references in server files and HTML index
    - Changed the script reference in index.server.html from main-DOMBJSBT.js to main-MPAW5HDO.js.
    - Updated import statement in main.server.mjs to use chunk-PS7SW7AX.mjs instead of chunk-KLY5XRCA.mjs.
    - Modified server.mjs to reflect the new chunk import for consistency across server files.

--- SignVision/SignVisionApp/dist/sign-vision-app/server/main.server.mjs
+++ SignVision/SignVisionApp/dist/sign-vision-app/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-KLY5XRCA.mjs";import"./chunk-RDPQP5ZW.mjs";import"./chunk-PPWFZELT.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-PS7SW7AX.mjs";import"./chunk-RDPQP5ZW.mjs";import"./chunk-PPWFZELT.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
